7e2dc71e8815a860e7e92cd67a3f36b016577c2b
virtumedia-agency <118538154+virtumedia-agency@users.noreply.github.com>
2024-04-20 08:56:26 +0200

    Update youtube-embed.js

--- youtube-embed.js
+++ youtube-embed.js
@@ -4,31 +4,36 @@
 <script>
 // Definicja globalnej zmiennej do przechowywania instancji odtwarzacza
 var player;
-console.log('Hello');
+
 // Funkcja wywoływana przez YouTube API po załadowaniu
 function onYouTubeIframeAPIReady() {
+    console.log('YouTube Iframe API is ready.');
     var iframe = document.querySelector('.iframe-fluid'); // Znajdź iframe z wideo
 
-    if (iframe && !player) {
+    if (iframe) {
+        console.log('Found iframe:', iframe);
         player = new YT.Player(iframe, {
             events: {
                 'onReady': onPlayerReady
             }
         });
+    } else {
+        console.error('Iframe not found.');
     }
 }
 
 // Funkcja wywoływana, gdy odtwarzacz jest gotowy
 function onPlayerReady(event) {
     console.log('YouTube player is ready.');
-
     var closeButton = document.querySelector('.link_close'); // Znajdź przycisk zamknięcia
 
     if (closeButton) {
+        console.log('Found close button:', closeButton);
         closeButton.addEventListener('click', function() {
             console.log('Close button clicked');
             if (player && player.stopVideo) {
                 player.stopVideo(); // Zatrzymaj wideo przy kliknięciu
+                console.log('Video stopped.');
             } else {
                 console.error('Player not initialized or stopVideo not available.');
             }
